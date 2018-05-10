c8c484ad79f9f6628e77d09917e076f0f15e0ab7
Joshua Tan <jtsarvida@gmail.com>
2018-05-10 19:37:34 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,2 +1,20 @@
 class Doctor 
+  
+  attr_accessor :name, :appointments
+  
+  def initialize(name)
+    @name = name
+    @appointments = []
+  end
+  
+  def add_appointment(appointment)
+    @appointments << appointment 
+  end
+  
+  def appointments
+    @appointments
+  end
+  
+  def patients(appointments)
+    
 end
