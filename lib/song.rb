e7a745ee1216a44c518832d2d35973c0de0cdd76
Joshua Tan <jtsarvida@gmail.com>
2018-05-10 19:14:50 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,11 @@
 class Song 
   
-  attr_accessor :name, :genre
+  attr_accessor :name, :genre, :artist
   
   def initialize(name, genre)
     @name = name
     @genre = genre
+    @artist
   end
   
   def add_song
