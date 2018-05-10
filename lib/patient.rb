b888def78264ba918633bb00ea3a4cfbacf76cba
Joshua Tan <jtsarvida@gmail.com>
2018-05-10 19:41:56 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,2 +1,12 @@
 class Patient
+  attr_accessor :name, :appointments
+  
+  def initialize(name)
+    @name = name
+    @appointments = []
+  end
+  
+  def add_appointment(appointment)
+    @appointments << appointment
+    appointment.self
 end
